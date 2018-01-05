6e7d3b12de90818cbbb0fdf61dd33bf29465bbf9
Steve Bentley <steve.bentley@chorus.co>
2018-01-05 14:11:05 +0000

    Add Jaeger Open Tracing

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt" 
+	"io"
+
     "golang.org/x/net/context"
     "google.golang.org/grpc"
     "github.com/tophatsteve/iaas/service"   
-    "fmt" 
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+	//"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"	
 )
 
 func main() {
+    tracer, closer := initTracer("iaas-client")
+    opentracing.SetGlobalTracer(tracer)
+    defer closer.Close()
+
 	conn, err := grpc.Dial(
 		"localhost:9191", 
 		grpc.WithInsecure(), 
@@ -18,8 +28,29 @@ func main() {
     defer conn.Close()
     
 	client := iaas.NewInsultSvcClient(conn)
-	ctx := context.Background()
-    respGenerate, err := client.Generate(ctx, &iaas.Empty{})
+    ctx := context.Background()
     
+    span := tracer.StartSpan("generate")
+    defer span.Finish()
+    respGenerate, err := client.Generate(ctx, &iaas.Empty{})
+    span.SetTag("response", respGenerate.Value)
+
     fmt.Println(respGenerate.Value)
+}
+
+func initTracer(service string) (opentracing.Tracer, io.Closer) {
+    cfg := &config.Configuration{
+        Sampler: &config.SamplerConfig{
+            Type:  "const",
+            Param: 1,
+        },
+        Reporter: &config.ReporterConfig{
+            LogSpans: true,
+        },
+    }
+    tracer, closer, err := cfg.New(service, config.Logger(jaeger.StdLogger))
+    if err != nil {
+        panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
+    }
+    return tracer, closer
 }
